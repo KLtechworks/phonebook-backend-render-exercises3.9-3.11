c5312797827090efc0d918c4ebec1f5d6cb26b58
KLtechworks <kelly.w3bd@gmail.com>
2025-12-02 20:00:14 +1100

    fixed 3.22

--- index.js
+++ index.js
@@ -134,8 +134,8 @@ const errorHandler = (error, request, response, next) => {
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 
-app.listen(PORT,  () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`backend phonebook running http://localhost:${PORT}`)
 })
--- models/person.js
+++ models/person.js
@@ -9,13 +9,13 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 mongoose.connect(url, { family: 4 })
 
-  // .then(result => {
-  //   console.log('connected to MongoDB')
-  // })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
   .catch(error => {
     console.log('error connecting to MongoDB:', error.message)
   })
-mongoose.connection.close()
+
 // const personSchema = new mongoose.Schema({
 //   name: String,
 //   number: String,
